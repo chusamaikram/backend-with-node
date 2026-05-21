571fe6cbb24842e9885b26ff5c401e0b8cff0aab
chusamaikram <usamachaudhary0345@gmail.com>
2026-05-21 14:34:02 +0500

    created user controller and first api route

--- yt-project/src/app.js
+++ yt-project/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser"
 
+
 const app = express()
 
 app.use(cors({
@@ -20,6 +21,11 @@ app.use(express.urlencoded({
 app.use(express.static("public"));
 
 app.use(cookieParser())
+
+//routes 
+import router from "./routes/user.routes.js";
+
+app.use("/api/v1/users", router)
 
 
 export { app }
